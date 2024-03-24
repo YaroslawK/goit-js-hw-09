752c8352f79fc667492703ea6ba366293218bfe2
YaroslawK <xxjokerxx933@gmail.com>
2024-03-24 12:10:02 +0200

    add2

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,10 @@
-console.log('Gallery');
+// Описаний в документації
+import simpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const lightbox = new simpleLightbox('.gallery a', {
+});
+
+console.log(simpleLightbox);
